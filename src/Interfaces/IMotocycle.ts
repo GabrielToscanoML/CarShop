57c0080485df62dffc2de95660358722c98eccee
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-04-27 10:50:44 -0300

    Criação da interface MotoCycle e Vehicles e testando se o extends funcionou

---
+++ src/Interfaces/IMotocycle.ts
@@ -0,0 +1,7 @@
+import IVehicle from './IVehicle';
+
+export type Category = 'Street' | 'Custom' | 'Trail';
+export default interface IMotocycle extends IVehicle{
+  category: Category;
+  engineCapacity: number;
+}
